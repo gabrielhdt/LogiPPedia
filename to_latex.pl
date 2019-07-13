1204be167803f6c87f11bda90b3b77d92e088fba
gabrielhdt <gabrielhondet@gmail.com>
2019-07-13 18:29:32 +0200

    more comments

--- to_latex.pl
+++ to_latex.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/env swipl
+%% Print ppterms to stdout as latex strings.  LaTeX strings may
+%% contain utf-8
 
 :- initialization(main, main).
 :- use_module(library(http/json)).
@@ -6,16 +8,12 @@
 
 jatom(null).
 
+%% Entry point, accepts one file name on argv
 main([Fname]) :-
-    echo(['Loading', Fname]),
     json_of(Fname, Jterm),
     is_json_term(Jterm, [null(jatom(null))]),
     pp(Jterm),
     nl.
-
-echo([]) :- nl.
-echo([Last]) :- !, write(Last), nl.
-echo([H|T]) :- write(H), write(' '), echo(T).
 
 %% [json_of(+Fname, -Jterm)] reads file with name [Fname] and creates
 %% json term [Jterm]
@@ -32,12 +30,17 @@ pp(['Const', Content]) :- pp_const(Content).
 pp(['Binder', Content]) :- pp_binder(Content).
 pp(['Var', Content]) :- pp_var(Content).
 
-%% prints term T with a space before
+%% sp_pp(+T) prints term T with a latex small space space before
 sp_pp(T) :- format('\\, ~@', [pp(T)]).
 
+%% pp_const prints a constant
+% No arguments version
 pp_const(json([c_symb=Csym, c_args=[]])) :- format('~a', [Csym]).
+% With arguments
 pp_const(json([c_symb=Csym, c_args=Carg])) :-
     format('\\left(~a~@\\right)', [Csym, pp_args(Carg)]).
+
+%% pp_var prints a bound variable
 % No arguments version
 pp_var(json([v_symb=Vsym, v_args=[]])) :-
     format('~a', [Vsym]).
@@ -45,10 +48,12 @@ pp_var(json([v_symb=Vsym, v_args=[]])) :-
 pp_var(json([v_symb=Vsym, v_args=Varg])) :-
     format('\\left(~a~@\\right)', [Vsym, pp_args(Varg)]).
 
-%% No annotation
+%% pp_binder(+Bd) prints the data Bd that was shipped as ['Binder', Bd].
+% No annotation
 pp_binder(json([b_symb=Bsym, bound=Boun,
                 annotation=jatom(null), body=Body])) :-
     format('\\left(~a ~a, ~@\\right)', [Bsym, Boun, pp(Body)]).
+% With annotation
 pp_binder(json([b_symb=Bsym, bound=Boun, annotation=Anno,
                 body=Body])) :-
     Anno = [_, _],
