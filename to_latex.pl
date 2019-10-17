6da024b4577c6a318dd0b5fe4337db2adcfdf64f
gabrielhdt <gabrielhondet@gmail.com>
2019-10-17 08:29:36 +0200

    [*] some refactoring

--- to_latex.pl
+++ to_latex.pl
@@ -15,51 +15,49 @@ parse :-
     is_json_term(Jterm, []),
     pp(Jterm).
 
-%% [pp_args(+Ppts, -Ltx_ost)] concatenates list of ppterm
-%% arguments [Ppts] to a latex string [Ltx_ost].
-pp_args([]).
-pp_args(Ts) :- maplist(sp_pp, Ts).
-
 %% pp(+Pp) outputs ppterm [Pp] to stdout
 pp(['Const', Content]) :- pp_const(Content).
 pp(['Binder', Content]) :- pp_binder(Content).
 pp(['Var', Content]) :- pp_var(Content).
 
+%% [pp_args(+Ppts)] prints a list of arguments
+pp_args([]).
+pp_args(Ts) :- maplist(sp_pp, Ts).
+
 %% sp_pp(+T) prints term T with a latex small space space before
 sp_pp(T) :- format('\\, ~@', [pp(T)]).
 
-%% pp_const prints a constant
+%% pp_const(+Cst) prints constant Cst
 % No arguments version
 pp_const(json([c_symb=Csym, c_args=[]])) :- format('~a', [Csym]).
 % With arguments
 pp_const(json([c_symb=Csym, c_args=Carg])) :-
     format('\\left(~a~@\\right)', [Csym, pp_args(Carg)]).
 
-%% pp_var prints a bound variable
+%% pp_var(+V) prints a bound variable
 % No arguments version
-pp_var(json([v_symb=Vsym, v_args=[]])) :-
-    format('~a', [Vsym]).
+pp_var(json([v_symb=Vsym, v_args=[]])) :- format('~a', [Vsym]).
 % with arguments
 pp_var(json([v_symb=Vsym, v_args=Varg])) :-
     format('\\left(~a~@\\right)', [Vsym, pp_args(Varg)]).
 
+pp_annot(@(null)) :- format('').
+pp_annot(Term) :- format(': ~@', [pp(Term)]).
+
 %% pp_binder(+Bd) prints the data Bd that was shipped as ['Binder', Bd].
-% No annotation
+% Without arguments
 pp_binder(json([b_symb=Bsym,
                 bound=Boun,
-                annotation= @(null),
+                annotation=Anno,
                 body=Body,
-                b_args=Bargs])) :-
-    format('\\left(\\left(~a ~a, ~@\\right)~@\\right)',
-           [Bsym, Boun, pp(Body), pp_args(Bargs)]).
-% With annotation
+                b_args=[]])) :-
+    format('\\left(~a ~a~@, ~@\\right)',
+           [Bsym, Boun, pp_annot(Anno), pp(Body)]).
+% With arguments
 pp_binder(json([b_symb=Bsym,
                 bound=Boun,
                 annotation=Anno,
                 body=Body,
                 b_args=Bargs])) :-
-    Anno = [_, _],
-    format('\\left(\\left(~a ~a: ~@, ~@\\right)~@\\right)',
-           [Bsym, Boun, pp(Anno), pp(Body), pp_args(Bargs)]).
-
-% FIXME pp_binder contains superfluous parens if b_args is empty
+    format('\\left(\\left(~a ~a~@, ~@\\right)~@\\right)',
+           [Bsym, Boun, pp_annot(Anno), pp(Body), pp_args(Bargs)]).
