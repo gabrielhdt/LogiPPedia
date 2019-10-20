d8ef69156f8e79b252dd8b4c6bb7ec1a37372d4a
gabrielhdt <gabrielhondet@gmail.com>
2019-10-20 18:08:27 +0200

    [*] display directly, do not use strings

--- scheme/logipp/latex-pp.scm
+++ scheme/logipp/latex-pp.scm
@@ -22,34 +22,42 @@ first lexicographically)."
 
 (define (pp-args ts)
   "Prints ts as a list of arguments."
-  (let* ((space-pp
-          (lambda (_ t)
-            (string-concatenate/shared `("\\ " ,(pp t)))))
-         (spaced (vector-map space-pp ts)))
-    (string-concatenate/shared (vector->list spaced))))
+  (let ((space-pp
+         (lambda (_ t)
+           (begin
+             (display "\\ ")
+             (pp t)))))
+    (vector-for-each space-pp ts)))
 
 (define (pp-annot annot)
   "Prints annot as an annotation, that is ': annot'."
   (match annot
-    (#nil "")
-    (t (string-concatenate/shared `(": " ,(pp t))))))
+    (#nil #nil)
+    (t (begin
+         (display ": ")
+         (pp t)))))
 
 (define (pp-const const)
   "Prints constant ct with symbol c as '(c args)'"
   (match (normalise-object const)
     ((( "c_args" . #() ) ( "c_symb" . csym ))
-     (extras:ref-or-id bindings csym))
+     (display (extras:ref-or-id bindings csym)))
     ((( "c_args" . cargs ) ( "c_symb" . csym ))
-     (string-concatenate/shared `("\\left(" ,csym ,(pp-args cargs) "\\right)")))))
+     (begin
+       (format #t "\\left(~a" csym)
+       (pp-args cargs)
+       (display "\\right")))))
 
 (define (pp-var var)
   "Prints variable v of symbol v as '(v args)'"
   (match (normalise-object var)
     ((( "v_args" . #() ) ( "v_symb" . vsym ))
-     vsym)
+     (display vsym))
     ((( "v_args" . vargs ) ( "v_symb" . vsym ))
-     (string-concatenate/shared
-      `("\\left(" ,vsym ,(pp-args vargs) "\\right)")))))
+     (begin
+       (format #t "\\left(~a" vsym)
+       (pp-args vargs)
+       (display "\\right)")))))
 
 (define (pp-binder binder)
   "Given a binder with symbol B, bound variable x and body t, prints 'B x.t'"
@@ -59,17 +67,25 @@ first lexicographically)."
       ( "b_symb" . symb )
       ( "body" . t )
       ( "bound" . bound ))
-     (string-concatenate/shared
-      `("\\left(" ,symb " " ,bound ,(pp-annot anno) ", "
-        ,(pp t) "\\right)")))
+     (begin
+       (format #t "\\left(~a ~a" symb bound)
+       (pp-annot anno)
+       (display ", ")
+       (pp t)
+       (display "\\right)")))
     ((( "annotation" . anno )
       ( "b_args" . args )
       ( "b_symb" . symb )
       ( "bound" . bound )
       ( "body" . t ))
-     (string-concatenate/shared
-      `("\\left(\\left(" ,symb " " ,bound ,(pp-annot anno) ", "
-        ,(pp t) "\\right)" ,(pp-args args) "\\right)")))))
+     (begin
+       (format #t "\\left(\\left(~a ~a" symb bound)
+       (pp-annot anno)
+       (display ", ")
+       (pp t)
+       (display "\\right)")
+       (pp-args args)
+       (display "\\right)")))))
 
 ;;
 ;; Public procedure
