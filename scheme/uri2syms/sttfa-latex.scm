21039cb7086c880dbc9c2927b99d70faed583e20
gabrielhdt <gabrielhondet@gmail.com>
2019-10-24 09:05:57 +0200

    [+] mapping from sttfa to latex

---
+++ scheme/uri2syms/sttfa-latex.scm
@@ -0,0 +1,8 @@
+;; Mappings from sttfa uris to latex symbols
+(( "sttfa:sttfa/etap.cst"    . "\eta" )
+ ( "sttfa:sttfa/arrow.cst"   . "\to" )
+ ( "sttfa:sttfa/bool.cst"    . "\mathbb{B}" )
+ ( "sttfa:sttfa/eps.cst"     . "\epsilon" )
+ ( "sttfa:sttfa/impl.cst"    . "\Rightarrow" )
+ ( "sttfa:sttfa/forallP.cst" . "\forall_P" )
+ ( "stffa:sttfa/forallK.cst" . "\forall_K" ))
